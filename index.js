b48cc77e9dc837c57b6a4d7d26b72fdba585d182
benNjogu <kamandemachafi@gmail.com>
2022-08-23 14:36:02 +0300

    Without destructuring

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const colors = ["red", "green", "blue"];
-const items = colors.map((color) => `<li>${color}</li>`);
-console.log(items);
+const address = {
+  street: "Mackenzie",
+  city: "Kanairo",
+  country: "Kenya",
+};
+
+const street = address.street;
+const city = address.city;
+const country = address.country;
