7877cd0a8078a2437e8618e563a07bb649a49876
benNjogu <kamandemachafi@gmail.com>
2022-08-23 12:21:07 +0300

    var keyword

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function sayHello() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i);
+  }
 
+  //i is still accessible outside the for loop! shouldn't be.
+  console.log(i);
+}
+
+sayHello();
