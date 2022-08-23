aa78745f1fdf7e7ab018395a02b4278c37252c19
benNjogu <kamandemachafi@gmail.com>
2022-08-23 14:38:56 +0300

    destruction part 1

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const address = {
   country: "Kenya",
 };
 
-const street = address.street;
-const city = address.city;
-const country = address.country;
+const { street, city, country } = address;
+
+console.log(street);
