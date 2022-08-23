9ab9446a2694e94a01adbecfdc19b38632aadb4d
benNjogu <kamandemachafi@gmail.com>
2022-08-23 12:24:51 +0300

    const keyword

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function sayHello() {
-  for (let i = 0; i < 5; i++) {
-    console.log(i);
-  }
-
-  //with let, it is no longer accessible
-  console.log(i);
-}
-
-sayHello();
+const x = 1;
+x = 2;
+console.log(x);//error you can't re-assign a constant.
