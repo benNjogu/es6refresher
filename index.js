3ba1809dccea01e71e70399a6d103bfefb314020
benNjogu <kamandemachafi@gmail.com>
2022-08-23 21:47:04 +0300

    fix fizz buzz

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) console.log("fizz buzz");
   else if (i % 3 == 0) console.log("fizz");
   else if (i % 5 == 0) console.log("buzz");
-  console.log(i);
+  else console.log(i);
 }
