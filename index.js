f6664fbe61b68da69f18efd8b93eb2d655669026
benNjogu <kamandemachafi@gmail.com>
2022-08-24 13:23:53 +0300

    assigning a parameter

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function minus(a, b) {
-  if (b == undefined) return -a;
-  else return a - b;
-}
+const power = (base, exponent = 2) => {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
 
-console.log(minus(10));
-console.log(minus(10, 5));
+  return result;
+};
+
+console.log(power(4));
+console.log(power(2, 6));
+
+/**
+ * If you write an equals operator after a parameter, followed by an expression, the value of that expression will
+ * replace the argument when it is not given as illustrated above.
+ */
