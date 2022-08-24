a2e51e960a7f27a0b354375f2f9169c5543b64d0
benNjogu <kamandemachafi@gmail.com>
2022-08-24 20:59:53 +0300

    recursion

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-function multiplier(factor) {
-  return (number) => number * factor;
+/*const power = (base, exponent) => {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+
+  return result;
+};**/
+
+function power(base, exponent) {
+  if (exponent == 0) {
+    return 1;
+  }
+
+  return base * power(base, exponent - 1);
 }
 
-let twice = multiplier(7);
-console.log(twice(5));
+console.log(power(5, 2)); //->25
