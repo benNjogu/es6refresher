6058b7159739f8926162330b5beb24851f5e01c2
benNjogu <kamandemachafi@gmail.com>
2022-08-24 09:22:13 +0300

    Chess board

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0) console.log("fizz buzz");
-  else if (i % 3 == 0) console.log("fizz");
-  else if (i % 5 == 0) console.log("buzz");
-  else console.log(i);
+let size = 8;
+let char = " ";
+for (j = 0; j < size; j++) {
+  char += "\n";
+  for (i = 0; i < size; i++) {
+    if ((i + j) % 2 == 0) {
+      char += "#";
+    } else {
+      char += " ";
+    }
+  }
 }
+
+console.log(char);
