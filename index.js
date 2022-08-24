7d0f40ade5b1ec9bd71e9afa6f763edcaddc1bbb
benNjogu <kamandemachafi@gmail.com>
2022-08-24 11:13:41 +0300

    a function with a parameter

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-let size = 8;
-let char = "";
-for (let j = 0; j < size; j++) {
-  for (let i = 0; i < size - j; i++) {
-    char += " ";
-  }
-  for (let k = 0; k <= 2 * j; k++) {
-    char += "*";
-  }
-  char += "\n";
-}
-
-console.log(char);
+const square = function (side) {
+  return side * side;
+};
+console.log(square(5)); //->25
