d2af8d8fb23dfb41c00de0f90e1be6bb092b6be8
benNjogu <kamandemachafi@gmail.com>
2022-08-24 10:54:32 +0300

    downward triangle star pattern

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let size = 8;
 let char = "";
 for (let j = 0; j < size; j++) {
-  for (let k = 0; k <= j; k++) {
+  for (let k = size; k > j; k--) {
     char += "*";
   }
   for (let i = 0; i < size - j; i++) {
