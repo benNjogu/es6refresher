29e68037a8095f7ce046d8fa69121cee037e0917
benNjogu <kamandemachafi@gmail.com>
2022-08-24 13:41:44 +0300

    closure

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const power = (base, exponent = 2) => {
-  let result = 1;
-  for (let count = 0; count < exponent; count++) {
-    result *= base;
-  }
+function multiplier(factor) {
+  return (number) => number * factor;
+}
 
-  return result;
-};
-
-console.log(power(4));
-console.log(power(2, 6));
-
-/**
- * If you write an equals operator after a parameter, followed by an expression, the value of that expression will
- * replace the argument when it is not given as illustrated above.
- */
+let twice = multiplier(7);
+console.log(twice(5));
