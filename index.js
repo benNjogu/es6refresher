f42defa105c87cab0266dfebf836a3f151fa2906
benNjogu <kamandemachafi@gmail.com>
2022-08-24 09:40:56 +0300

    Chess board 2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let size = 8;
-let char = " ";
+let char = "";
 for (j = 0; j < size; j++) {
-  char += "\n";
   for (i = 0; i < size; i++) {
     if ((i + j) % 2 == 0) {
-      char += "#";
-    } else {
       char += " ";
+    } else {
+      char += "#";
     }
   }
+  char += "\n";
 }
 
 console.log(char);
