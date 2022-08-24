386b170adc4426280ba6af8f0cf137426282d96b
benNjogu <kamandemachafi@gmail.com>
2022-08-24 10:42:27 +0300

    left triangle

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let size = 8;
 let char = "";
 for (let j = 0; j < size; j++) {
-  for (let i = 0; i < size - j; i++) {
-    char += " ";
-  }
   for (let k = 0; k <= j; k++) {
     char += "*";
   }
+  for (let i = 0; i < size - j; i++) {
+    char += " ";
+  }
+  
   char += "\n";
 }
 
