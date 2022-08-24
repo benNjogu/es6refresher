24b7bea2fa08447f2cd06f4d796e95e913922702
benNjogu <kamandemachafi@gmail.com>
2022-08-24 12:14:42 +0300

    fuction declaration

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-let missileSystem = {
-  safeMode: true,
-  lauch(time) {
-    console.log("lauching " + time);
-  },
-};
-
-let lauchMissiles = function () {
-  missileSystem.lauch("now");
-};
-
-safeMode = missileSystem.safeMode;
-if (safeMode) {
-  lauchMissiles = function () {
-    console.log("can't lauch, safe mode enabled");
-  };
+function square(side) {
+  return side * side;
 }
 
-lauchMissiles();
+console.log(square(4));
