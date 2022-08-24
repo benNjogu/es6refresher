b7f32c488094ccebebc7eb7c167283cb69dab656
benNjogu <kamandemachafi@gmail.com>
2022-08-24 10:38:21 +0300

    right triangle

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 let size = 8;
 let char = "";
-for (let j = 0; j <= size; j++) {
-  for (let i = 0; i <= size; i++) {
-    if (i === 0 || i === size) {
-      char += "#";
-    } else if (j === 0 || j === size) {
-      char += "#";
-    } else {
-      char += " ";
-    }
+for (let j = 0; j < size; j++) {
+  for (let i = 0; i < size - j; i++) {
+    char += " ";
+  }
+  for (let k = 0; k <= j; k++) {
+    char += "*";
   }
   char += "\n";
 }
