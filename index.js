8ffe313223bd842d3bd150eea9f51181c524f201
benNjogu <kamandemachafi@gmail.com>
2022-08-24 13:05:37 +0300

    The call stack(pg 45)

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const power = (base, exponent) => {
-  let result = 1;
-  for (let count = 0; count < exponent; count++) {
-    result *= base;
-  }
+function greet(who) {
+  console.log("Hello " + who);
+}
 
-  return result;
-};
-
-console.log(power(15, 2));
-
-let area = (side) => power(side, 2);
-
-console.log(area(51));
-
-const horn = () => console.log("hoot");
-
-horn();
+greet("Harry");
+console.log("Bye");
