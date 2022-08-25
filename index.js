0793224b8526f2519c8c0747cc9b47eb2f81ff33
benNjogu <kamandemachafi@gmail.com>
2022-08-25 15:38:15 +0300

    Object keys

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 let anObject = {
-  left: 1,
-  right: 2,
+  x: 1,
+  y: 2,
+  z: "naah!",
 };
 
-console.log(anObject.left); // 1
-delete anObject.left; //deletes the specified object
-console.log(anObject.left); //undefined
-console.log("left" in anObject); //false
-console.log("right" in anObject); //true
-
-/**
- * The binary in operator when applied to a string and an object, tells you
- * whether that object has a property with that name.
- */
+console.log(Object.keys(anObject));//->[ "x", "y", "z" ]
