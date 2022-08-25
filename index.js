3bf6097b20ca135337e9dc03d58ba8db8571a67b
benNjogu <kamandemachafi@gmail.com>
2022-08-25 10:29:41 +0300

    naming challenge

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function printFarmInventory(cows, chicken) {
-  let cowString = String(cows);
-  while (cowString.length < 3) {
-    cowString = "0" + cowString;
+function printZeroPaddedWithLabel(number, label) {
+  let numberString = String(number);
+  while (numberString.length < 3) {
+    numberString = "0" + numberString;
   }
-  console.log(`${cowString} cows`);
-  let chickenString = String(chicken);
-  while (chickenString.length < 3) {
-    chickenString = "0" + chickenString;
-  }
-  console.log(`${chickenString} chicken`);
+
+  console.log(`${numberString} ${label}`);
 }
 
-printFarmInventory(7, 11);
-//Above, the farmer keeps cows and chicken. but if he adds pigs, we repeat the four lines of code which 
-//creates repetition.
+function printFarmInventory(cows, chicken, pigs) {
+  printZeroPaddedWithLabel(cows, "Cows");
+  printZeroPaddedWithLabel(chicken, "Chicken");
+  printZeroPaddedWithLabel(pigs, "Pigs");
+}
+
+printFarmInventory(7, 11, 3);
