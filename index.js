8031edfeec1bdce48b1bd28a81763d5d7044442f
benNjogu <kamandemachafi@gmail.com>
2022-08-25 14:53:55 +0300

    An array

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-function countChar(string, char) {
-  let count = 0;
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] == char) {
-      count++;
-    }
-  }
-
-  return count;
-}
-let letter = "B";
-console.log(`There are ${countChar("BARNABAS", letter)} ${letter}s`);//->There are 2 Bs
+let listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[1]);
