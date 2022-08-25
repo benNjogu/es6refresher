9d25100a19dbb1bb75bb35a1fc923c7478fe5507
benNjogu <kamandemachafi@gmail.com>
2022-08-25 05:56:22 +0300

    A recursion puzzle

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-/*const power = (base, exponent) => {
-  let result = 1;
-  for (let count = 0; count < exponent; count++) {
-    result *= base;
+/**
+ * A puzzle:
+ * By starting from the number 1 and repeatedly either adding 5 or multiplying by 3, an infinite set of numbers
+ * can be produced. How would you write a function that given a number, tries to find a sequence of such additions
+ * and multiplications that produces that number.
+ * e.g. 13 could be reached by first multiplying by 3 and adding 5 twice. 15 can not be reached at all.
+ */
+function findSoln(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return (
+        find(current + 5, `(${history} + 5)`) ||
+        find(current * 3, `(${history} * 3)`)
+      );
+    }
   }
 
-  return result;
-};**/
-
-function power(base, exponent) {
-  if (exponent == 0) {
-    return 1;
-  }
-
-  return base * power(base, exponent - 1);
+  return find(1, "1");
 }
 
-console.log(power(5, 2)); //->25
+console.log(findSoln(13)); //->(((1 * 3) + 5) + 5)
