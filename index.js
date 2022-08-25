2eb622939203aef4f020279d5384d002e8ad5387
benNjogu <kamandemachafi@gmail.com>
2022-08-25 15:07:33 +0300

    push and pop methods

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-let listOfNumbers = [2, 3, 5, 7, 11];
-console.log(listOfNumbers["length"]);//5
-console.log(listOfNumbers.length);//5
+let sequence = [1, 2, 3, 4];
+sequence.push(5);
+sequence.push(6); //adds an element at the end of an array
+
+console.log(sequence); //-> [ 1, 2, 3, 4, 5, 6 ]
+console.log(sequence.pop()); //-> 6 //removes the last element of an array and returns it
