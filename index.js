d17b3f4b84eb19aa66c948b9dbf7bf89cc7807d2
benNjogu <kamandemachafi@gmail.com>
2022-08-25 15:12:11 +0300

    an object

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let sequence = [1, 2, 3, 4];
-sequence.push(5);
-sequence.push(6); //adds an element at the end of an array
+let day1 = {
+  squirrel: false,
+  events: ["work", "touched tree", "pizza", "running"],
+};
 
-console.log(sequence); //-> [ 1, 2, 3, 4, 5, 6 ]
-console.log(sequence.pop()); //-> 6 //removes the last element of an array and returns it
+console.log(day1.squirrel); //false
+console.log(day1.events[2]); //pizza
