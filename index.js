789932f600145da0b8904a01a394dfbbec2321d2
benNjogu <kamandemachafi@gmail.com>
2022-08-25 10:20:03 +0300

    repetition

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-/**
- * A puzzle:
- * By starting from the number 1 and repeatedly either adding 5 or multiplying by 3, an infinite set of numbers
- * can be produced. How would you write a function that given a number, tries to find a sequence of such additions
- * and multiplications that produces that number.
- * e.g. 13 could be reached by first multiplying by 3 and adding 5 twice. 15 can not be reached at all.
- */
-function findSoln(target) {
-  function find(current, history) {
-    if (current == target) {
-      return history;
-    } else if (current > target) {
-      return null;
-    } else {
-      return (
-        find(current + 5, `(${history} + 5)`) ||
-        find(current * 3, `(${history} * 3)`)
-      );
-    }
+function printFarmInventory(cows, chicken) {
+  let cowString = String(cows);
+  while (cowString.length < 3) {
+    cowString = "0" + cowString;
   }
-
-  return find(1, "1");
+  console.log(`${cowString} cows`);
+  let chickenString = String(chicken);
+  while (chickenString.length < 3) {
+    chickenString = "0" + chickenString;
+  }
+  console.log(`${chickenString} chicken`);
 }
 
-console.log(findSoln(13)); //->(((1 * 3) + 5) + 5)
+printFarmInventory(7, 11);
+//Above, the farmer keeps cows and chicken. but if he adds pigs, we repeat the four lines of code which 
+//creates repetition.
