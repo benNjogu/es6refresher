a3388388883ba6a02861db00a6f506af66e70067
benNjogu <kamandemachafi@gmail.com>
2022-08-25 11:05:12 +0300

    Min challenge

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-function zeroPad(number, width) {
-  let string = String(number);
-  while (string.length < width) {
-    string = "0" + string;
-  }
-  return string;
+function min(num1, num2) {
+  if (num1 < num2) return num1;
+  return num2;
 }
 
-function printFarmInventory(cows, chicken, pigs, goats) {
-  console.log(`${zeroPad(chicken, 3)}`);
-  console.log(`${zeroPad(pigs, 3)}`);
-  console.log(`${zeroPad(cows, 3)}`);
-  console.log(zeroPad(goats, 3));
-}
-
-printFarmInventory(9, 13, 5, 2);
+console.log(min(14, 6));
