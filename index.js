76e4ff50abe8efcab4a7472d476a8dd5110315bc
benNjogu <kamandemachafi@gmail.com>
2022-08-26 11:20:55 +0300

    for of loop

--- index.js
+++ index.js
@@ -25,3 +25,10 @@ for (let i = 0; i < journal.length; i++) {
   console.log(entry.events.length); //-> 5 6 5
 }
 
+for (let entry of journal) {
+  console.log(`${entry.events.length} events.`);
+}
+/**
+ * when a for loop looks like this, with the word of after a variable defination,
+ * it will loop over the elements of the value given after of.
+ */
