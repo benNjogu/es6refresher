529d24b7c5adf7c28e6bfa8a71353dd163dbf89e
benNjogu <kamandemachafi@gmail.com>
2022-08-26 15:48:21 +0300

    spreading an array in an array

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ console.log(max(4, 1, 9, -2)); //-> 9
 let numbers = [5, 1, 7];
 console.log(max(...numbers)); //-> 7
 console.log(...numbers);//-> 5 1 7
+/**
+ * The above spreads out the array into the function call, passing its elements as separate arguments.
+ * It is possible to include an array like that along with other arguments, as in max(9, ...numbers, 2)
+ */
+//for example;
+let words = ['never', 'fully'];
+console.log(['will', ...words, 'understand']);//-> [ "will", "never", "fully", "understand" ]
