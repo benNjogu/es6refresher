e6c0a46fae9061a934e2146ce1603cd8a201c569
benNjogu <kamandemachafi@gmail.com>
2022-08-26 11:50:47 +0300

    indexOf and lastIndexOf

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-let todoList = [];
-function remember(task) {
-  todoList.push(task); //adds an item at the back of the queue
-}
+console.log([1, 2, 3, 2, 1].indexOf(2)); //->1
 
-function getTask() {
-  return todoList.shift(); //shift gets and removes the front item of the queue
-}
-
-function rememberUrgently(task) {
-  todoList.unshift(task); //unshift adds an item but at the top of the queue
-}
-
-remember("groceries");
-remember("weed");
-remember("rice");
-remember("frolence");
-remember("dayaanaaah");
-
-console.log(getTask());
-console.log(getTask());
-console.log(todoList.length);
-
-rememberUrgently("the prince");
-console.log(todoList[0]);
+console.log([1, 2, 3, 2, 1].lastIndexOf(2)); //->3
