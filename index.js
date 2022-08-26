1adb3b1a3621bb351929f3c3e5e3026ae5b40bff
benNjogu <kamandemachafi@gmail.com>
2022-08-26 17:53:46 +0300

    destructuring

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-console.log(Math.abs(-56)); //-> 56
-/**
- * The above will negate the negative value, but would leave a positive one as is.
- */
+let details = {
+  name: "faraj",
+  age: 23,
+};
+
+let { name, age: id } = details; //in this case id is an alias
+
+console.log(id); //-> 23
