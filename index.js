8bf65577d4afea6b0cdcd9d6414ac5aa303e81e1
benNjogu <kamandemachafi@gmail.com>
2022-08-26 16:01:20 +0300

    Math functions(random, cos, sin, PI)

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-function max(...numbers) {
-  let result = -Infinity;
-  for (let number of numbers) {
-    if (number > result) result = number;
-  }
-
-  return result;
+function randomPointOnCircle(radius) {
+  let angle = Math.random() * 2 * Math.PI;
+  return { x: radius * Math.cos(angle), y: radius * Math.sin(angle) };
 }
 
-console.log(max(4, 1, 9, -2)); //-> 9
-
-let numbers = [5, 1, 7];
-console.log(max(...numbers)); //-> 7
-console.log(...numbers);//-> 5 1 7
-/**
- * The above spreads out the array into the function call, passing its elements as separate arguments.
- * It is possible to include an array like that along with other arguments, as in max(9, ...numbers, 2)
- */
-//for example;
-let words = ['never', 'fully'];
-console.log(['will', ...words, 'understand']);//-> [ "will", "never", "fully", "understand" ]
+console.log(randomPointOnCircle(2)); //-> { x: 1.4283266026152281, y: -1.3999582551853609 }
