22292ae92ea196378c5a3c1c3a67dcb83f90d268
benNjogu <kamandemachafi@gmail.com>
2022-08-26 10:32:45 +0300

    calculating phi describes Math.sqrt()

--- index.js
+++ index.js
@@ -26,3 +26,17 @@ function addEntry(events, squirrel) {
 
 addEntry(["hope", "this", "will", "work"], false);
 console.log(journal[3]);
+
+function phi(table) {
+  return (
+    (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt(
+      (table[2] + table[3]) *
+        (table[0] + table[1]) *
+        (table[1] + table[3]) *
+        (table[0] + table[2])
+    )
+  );
+}
+//The above as as per the formulae on page 66 Eloquent Javascript
+console.log(phi([76, 9, 4, 1])); //-> 0.06859943405700354
