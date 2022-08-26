a85a4ab35d6415cff8f566550c8784156c78627e
benNjogu <kamandemachafi@gmail.com>
2022-08-26 12:18:25 +0300

    slice method

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-console.log([1, 2, 3, 2, 1].indexOf(2)); //->1
+console.log([1, 2, 3, 2, 1].slice(2, 4)); //->[ 3, 2 ]
 
-console.log([1, 2, 3, 2, 1].lastIndexOf(2)); //->3
+console.log([1, 2, 3, 2, 1].slice(2)); //->[ 3, 2, 1 ]
+/**
+ * Slice takes the start and end indices and returns an array that has only the elements btn them.
+ * The start index is inclusive, the end index is exclusive.
+ * When the end index is not given, slice will take all the elements after the start index.
+ */
