9461e53c55b7c7755ca24e638cdb5e70a7b62f5a
benNjogu <kamandemachafi@gmail.com>
2022-08-26 16:14:13 +0300

    Math.random

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function randomPointOnCircle(radius) {
-  let angle = Math.random() * 2 * Math.PI;
-  return { x: radius * Math.cos(angle), y: radius * Math.sin(angle) };
-}
-
-console.log(randomPointOnCircle(2)); //-> { x: 1.4283266026152281, y: -1.3999582551853609 }
+console.log(Math.random());
+/**
+ * returns new pseudorandom number between 0 (inclusive) and 1 (exclusive), everytime you call it.
+ */
