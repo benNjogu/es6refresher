17b57a8341ce9e515e9ab46cdac1eba3fc5398a2
benNjogu <kamandemachafi@gmail.com>
2022-08-26 13:08:17 +0300

    repeat method

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-let sentence = "Secretary birds specialize in stomping";
-let words = sentence.split(" ");
-console.log(words); //-> [ "Secretary", "birds", "specialize", "in", "stomping" ]
-
-console.log(words.join(". ")); //-> Secretary. birds. specialize. in. stomping
-console.log(words.join(" ") + ".");//-> Secretary birds specialize in stomping.
+let string = "LA";
+console.log(string.repeat(3)); //-> LALALA
