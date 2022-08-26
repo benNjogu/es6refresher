ea7647c62574a223991efba02767ff48e3040962
benNjogu <kamandemachafi@gmail.com>
2022-08-26 13:06:25 +0300

    Join strings

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let sentence = "Secretary birds specialize in stomping";
 let words = sentence.split(" ");
 console.log(words); //-> [ "Secretary", "birds", "specialize", "in", "stomping" ]
+
+console.log(words.join(". ")); //-> Secretary. birds. specialize. in. stomping
+console.log(words.join(" ") + ".");//-> Secretary birds specialize in stomping.
