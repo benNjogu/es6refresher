ba8d526fbfb37c8a8502c6c30d5acf059d131979
benNjogu <kamandemachafi@gmail.com>
2022-08-26 11:40:52 +0300

    A todo list. describe shift and unshift methods

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
-let journal = [
-  {
-    events: ["work", "touched tree", "pizza", "running", "television"],
-    squirrel: false,
-  },
-  {
-    events: [
-      "work",
-      "ice cream",
-      "cauliflower",
-      "lasagna",
-      "touched tree",
-      "brushed teeth",
-    ],
-    squirrel: false,
-  },
-  {
-    events: ["weekend", "cycling", "break", "peanuts", "beer"],
-    squirrel: true,
-  },
-];
-
-for (let i = 0; i < journal.length; i++) {
-  let entry = journal[i];
-  console.log(entry.events.length); //-> 5 6 5
+let todoList = [];
+function remember(task) {
+  todoList.push(task); //adds an item at the back of the queue
 }
 
-for (let entry of journal) {
-  console.log(`${entry.events.length} events.`);
+function getTask() {
+  return todoList.shift(); //shift gets and removes the front item of the queue
 }
-/**
- * when a for loop looks like this, with the word of after a variable defination,
- * it will loop over the elements of the value given after of.
- */
+
+function rememberUrgently(task) {
+  todoList.unshift(task); //unshift adds an item but at the top of the queue
+}
+
+remember("groceries");
+remember("weed");
+remember("rice");
+remember("frolence");
+remember("dayaanaaah");
+
+console.log(getTask());
+console.log(getTask());
+console.log(todoList.length);
+
+rememberUrgently("the prince");
+console.log(todoList[0]);
