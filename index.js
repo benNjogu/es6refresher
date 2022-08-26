cdc98cca00bf121a91613f1b3114ce3f061755a0
benNjogu <kamandemachafi@gmail.com>
2022-08-26 12:27:37 +0300

    array.concat method

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-console.log([1, 2, 3, 2, 1].slice(2, 4)); //->[ 3, 2 ]
+function remove(array, index) {
+  return array.slice(0, index).concat(array.slice(index + 1));
+}
 
-console.log([1, 2, 3, 2, 1].slice(2)); //->[ 3, 2, 1 ]
-/**
- * Slice takes the start and end indices and returns an array that has only the elements btn them.
- * The start index is inclusive, the end index is exclusive.
- * When the end index is not given, slice will take all the elements after the start index.
- */
+console.log(remove(["a", "b", "c", "d", "e"], 2)); //->[ "a", "b", "d", "e" ]
