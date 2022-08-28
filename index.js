484bfdfc6f5d5e5c80a9611c125b3592b88d10bb
benNjogu <kamandemachafi@gmail.com>
2022-08-28 15:12:14 +0300

    array.reduce

--- index.js
+++ index.js
@@ -73,13 +73,4 @@ let SCRIPTS = [
   },
 ];
 
-function reduce(array, combine, start) {
-  let current = start;
-  for (let element of array) {
-    current = combine(current, element);
-  }
-
-  return current;
-}
-
-console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 0));//-> 10
+console.log([1, 2, 3, 4].reduce((a, b) => a + b));//-> 10
