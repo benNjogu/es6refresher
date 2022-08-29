9b6edb2880c1203c8e5e9c5214ee18c67fc4670a
benNjogu <kamandemachafi@gmail.com>
2022-08-29 12:32:21 +0300

    A loop

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-let array1 = [
-  [1, 2, 3, 4, 5],
-  [6, 7, 8, 9],
-  [11, 12, 13, 14, 15],
-];
+let i = 0;
+function forLoop(value, testFunction, updateFunction, bodyFunction) {
+  if (i > value) {
+    return;
+  }
 
-console.log(array1.reduce((a, b) => a.concat(b))); //->  [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15]
+  testFunction(i, value);
+  bodyFunction(i);
+  i = updateFunction(i);
+
+  return forLoop(value, testFunction, updateFunction, bodyFunction);
+}
+
+forLoop(
+  5,
+  (a, d) => a <= d,
+  (b) => (b += 1),
+  (c) => console.log(c)
+);
+//-> 0, 1, 2, 3, 4, 5
