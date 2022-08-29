e9fc3ca8b2f8468633631105db8ffdf4dca6e0eb
benNjogu <kamandemachafi@gmail.com>
2022-08-29 13:29:47 +0300

    The this keyword

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ rabbit.speak("I am alive"); //-> The rabbit says 'I am alive'
  * Methods:
  * Properties that hold function values.
  */
+
+function speak(line) {
+  console.log(`The ${this.type} rabbit says '${line}'`);
+}
+
+let whiteRabbit = { type: "white", speak };
+let hungryRabbit = { type: "hungry", speak };
+
+whiteRabbit.speak("am fucking white.");//-> The white rabbit says 'am fucking white.'
+hungryRabbit.speak("i could use a carrot.");//-> The hungry rabbit says 'i could use a carrot.'
