3bd61aa77f4d9e30a0ac11bca24752dda6bc30e5
benNjogu <kamandemachafi@gmail.com>
2022-08-29 13:53:17 +0300

    function.call method

--- index.js
+++ index.js
@@ -11,11 +11,17 @@ rabbit.speak("I am alive"); //-> The rabbit says 'I am alive'
  */
 
 function speak(line) {
-  console.log(`The ${this.type} rabbit says '${line}'`);
+  console.log(`The ${this.type} ${this.cute} rabbit says '${line}'`);
 }
 
-let whiteRabbit = { type: "white", speak };
-let hungryRabbit = { type: "hungry", speak };
+let whiteRabbit = { type: "white", cute: "cute", speak };
+let hungryRabbit = { type: "hungry", cute: "cute", speak };
 
 whiteRabbit.speak("am fucking white.");//-> The white rabbit says 'am fucking white.'
 hungryRabbit.speak("i could use a carrot.");//-> The hungry rabbit says 'i could use a carrot.'
+
+speak.call(hungryRabbit, "am full");
+/**
+ * With the function call method above, the this value is taken as the first argument and treats further 
+ * arguments as normal parameters.
+ */
