20c433a7a14a901e72f5a9bb7501eb45b55aa2e4
benNjogu <kamandemachafi@gmail.com>
2022-08-29 20:38:27 +0300

    class in an expression.

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-class Rabbit {
-  constructor(type) {
-    this.type = type;
+let object = new (class {
+  getWord() {
+    return "hello";
   }
+})();
 
-  speak(line) {
-    console.log(`The ${this.type} rabbit says '${line}'`);
-  }
-}
-
-let killerRabbit = new Rabbit("killer");
-killerRabbit.speak("I will kill");
-
-let blackRabbit = new Rabbit("black");
-blackRabbit.speak("I love whites");
+console.log(object.getWord()); //-> hello
 
 /**
- * The above provides the actual constructor function, which will be bound to the name Rabbit.
- * The others are packaged into that constructors prototype.
- * Class declarations only allow methods to be added to the prototype. This is inconvinient when you want to
- * save a non-function value in there.
+ * As shown above, class can be used both in statements and in expressions
  */
