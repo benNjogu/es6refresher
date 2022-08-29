91edf4307cb3e75d3c59522cf9a5e362514b8047
benNjogu <kamandemachafi@gmail.com>
2022-08-29 14:39:55 +0300

    function and array prototypes

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ console.log(empty.toString()); //-> [object Object]
 
 console.log(Object.getPrototypeOf({}) == Object.prototype); //-> true
 console.log(Object.getPrototypeOf(Object.prototype)); //-> null
+
+/**
+ * Many objects don't directly have object.prototype as their prototype but instead have another
+ * object that provides a different set of default properties.
+ */
+//e.g.
+console.log(Object.getPrototypeOf(Math.max) == Function.prototype);//-> true
+console.log(Object.getPrototypeOf([]) == Array.prototype);//-> true
