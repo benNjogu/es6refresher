3157b2603212f7e63271d27353912b0f493b5527
benNjogu <kamandemachafi@gmail.com>
2022-08-30 11:58:47 +0300

    prototype.toString

--- index.js
+++ index.js
@@ -20,3 +20,20 @@ console.log(sym == Symbol("name")); //-> false
 
 Rabbit.prototype[sym] = 55;
 console.log(blackRabbit[sym]);//-> 55
+
+/**
+ * Multiple symbols can have the same name.
+ */
+const toStringSymbol = Symbol('toString');
+Array.prototype[toStringSymbol] = function(){
+  return `${this.length} cm of blue yarn`;
+};
+
+console.log([1, 2].toString());//-> 1,2
+console.log([1, 2][toStringSymbol]());//-> 2 cm of blue yarn
+
+
+
+
+
+
