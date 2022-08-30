ce88bf027b7bcb963a036f24198b52c8f4d855e1
benNjogu <kamandemachafi@gmail.com>
2022-08-30 05:19:58 +0300

    calling array.toString from Object prototype.

--- index.js
+++ index.js
@@ -24,3 +24,5 @@ console.log(Rabbit.prototype.teeth); //-> small
 
 console.log(Array.prototype.toString == Object.prototype.toString); //-> false
 console.log([1, 2].toString());//1, 2
+
+console.log(Object.prototype.toString.call([1, 2]));//-> [object Array]
