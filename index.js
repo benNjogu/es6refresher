48d92bc27bc0b8f7b7e445208d7b301b385eb9eb
benNjogu <kamandemachafi@gmail.com>
2022-08-30 18:20:42 +0300

    The instanceof operator

--- index.js
+++ index.js
@@ -68,3 +68,9 @@ class SymmetricMatrix extends Matrix {
 
 let matrix = new SymmetricMatrix(5, (x, y) => `${x}, ${y}`);
 console.log(matrix.get(2, 3)); //-> 3, 2
+
+//To check whether an object was derived from a specific class, we use a binary operator called instanceof
+console.log(new SymmetricMatrix(2) instanceof SymmetricMatrix);//-> true
+console.log(new SymmetricMatrix(2) instanceof Matrix);//-> true
+console.log(new Matrix(2, 2) instanceof SymmetricMatrix)//-> false
+console.log([1] instanceof Array);//-> true
