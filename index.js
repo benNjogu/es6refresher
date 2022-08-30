6a7901da0fb0c210569883a419728753a19d7bf8
benNjogu <kamandemachafi@gmail.com>
2022-08-30 06:02:36 +0300

    Symbols

--- index.js
+++ index.js
@@ -14,3 +14,9 @@ Rabbit.prototype.toString = function () {
 
 let blackRabbit = new Rabbit("black");
 console.log(String(blackRabbit)); //-> a black rabbit
+
+let sym = Symbol("name");
+console.log(sym == Symbol("name")); //-> false
+
+Rabbit.prototype[sym] = 55;
+console.log(blackRabbit[sym]);//-> 55
