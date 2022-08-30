8716e6bd4e2e5ec2ae1a27a7fd43443af32e0249
benNjogu <kamandemachafi@gmail.com>
2022-08-30 05:01:37 +0300

    Overriding derived properties

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-let object = new (class {
-  getWord() {
-    return "hello";
+class Rabbit {
+  constructor(type) {
+    this.type = type;
   }
-})();
 
-console.log(object.getWord()); //-> hello
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+}
+
+Rabbit.prototype.teeth = "small";
+let killerRabbit = new Rabbit("killer");
+console.log(killerRabbit.teeth); //-> small
+killerRabbit.teeth = "long, sharp, and bloody";
+console.log(killerRabbit.teeth); //-> long, sharp, and bloody
+let blackRabbit = new Rabbit("black");
+console.log(blackRabbit.teeth); //-> small
+console.log(Rabbit.prototype.teeth); //-> small
 
 /**
- * As shown above, class can be used both in statements and in expressions
+ * When you add a property to an object, whether it is present in the prototype or not,
+ * the property is added to the object itself.
  */
