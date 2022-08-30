9f5463e4b728530ce5cf5590ce1801e9348c1052
benNjogu <kamandemachafi@gmail.com>
2022-08-30 17:34:01 +0300

    Degree celcius to fahrenheit and viceversa

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-let varyingSize = {
-  get size() {
-    return Math.floor(Math.random() * 100);
-  },
-};
+class Temparature {
+  constructor(celcius) {
+    this.celcius = celcius;
+  }
 
-console.log(varyingSize.size); //-> 53
+  get fahrenheit() {
+    return this.celcius * 1.8 + 32;
+  }
 
-console.log(varyingSize.size); //-> 57
+  set fahrenheit(value) {
+    this.celcius = (value - 32) / 1.8;
+  }
+
+  //Methods that have static written b4 them are stored in the constructor.
+  static fromFahrenheit(value) {
+    return new Temparature((value - 32) / 1.8);
+  }
+}
+
+let temp = new Temparature(22);
+console.log(temp.fahrenheit); //-> 71.6
+temp.fahrenheit = 71.6;
+console.log(temp.celcius); //-> 21.999999999999996 ~ 22
+console.log(Temparature.fromFahrenheit(71.6)); //-> { celcius: 21.999999999999996 }
