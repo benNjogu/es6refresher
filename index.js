66ce9a70c481072301dcd1ce1bacee77e6a02a36
benNjogu <kamandemachafi@gmail.com>
2022-09-02 11:11:51 +0300

    testing

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 /**
- * You can add a comment before a function to describe its type.
- * 
- * On others you can introduce a type variable T, which can stand in for any type, 
- * e.g ([T]) -> T(function from an array of Ts to a T).
+ * Automated testing is the process of writing a program that tests another program
  */
+
+function test(label, body){
+  if(!body()) console.log(`Failed: ${label}`);
+}
+
+test("Convert Latin text to uppercase", () => {
+  return "hello".toUpperCase() == "HELLO"
+});
+
+test("Convert Greek text to uppercase", () => {
+  return "Χαίρετε".toUpperCase() == "XAIPETE"
+});//-> Failed: Convert Greek text to uppercase
+
+console.log("Χαίρετε".toUpperCase());//-> ΧΑΊΡΕΤΕ
