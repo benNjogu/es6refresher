000122b2c6ff8635062053cba4a9e0b2b3d54d47
benNjogu <kamandemachafi@gmail.com>
2022-09-02 10:40:59 +0300

    Types

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
 /**
- * Bugs - flows in a computer program.
- * Debugging - the process of finding bugs.
+ * You can add a comment before a function to describe its type.
+ * 
+ * On others you can introduce a type variable T, which can stand in for any type, 
+ * e.g ([T]) -> T(function from an array of Ts to a T).
  */
-"use strict";
-
-function canYouSportTheProblem() {
-  "use strict";
-  for (let counter = 0; counter < 10; counter++) {
-    console.log("Happy life");
-  }
-}
-
-canYouSportTheProblem(); //-> Uncaught ReferenceError: assignment to undeclared variable counter
-
-function Person(name) {
-  this.name = name;
-}
-
-//without strict mode the code below works
-//let ferdinand = Person("Ferdinand");
-//console.log(name); //-> Ferdinand
-
-//let ferdinand = Person("Ferdinand");
-//console.log(name); //-> Uncaught TypeError: this is undefined
-
-let ferdinad = new Person("Ferdinand");
-console.log(name); //-> Ferdinand
