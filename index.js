0f43350003973570ba2659a357bedb9e4360fee3
benNjogu <kamandemachafi@gmail.com>
2022-09-02 12:30:20 +0300

    error propagation part 2

--- index.js
+++ index.js
@@ -4,4 +4,20 @@ function promptNumber(question) {
   else return result;
 }
 
-console.log(promptNumber("What did you score in your favourite unit?")); //-> 77
+//console.log(promptNumber("What did you score in your favourite unit?")); //-> 77
+
+function lastElement(array){
+  if(array.length == 0){
+    return {failed: true};
+  }else{
+    return {element: array[array.length - 1]}
+  }
+}
+
+console.log(lastElement([]));//-> { failed: true }
+console.log(lastElement([1, 2, 3, 4]));//-> { element: 4 }
+
+/**
+ * If the function can already return any kind of value, you'll have to do something like wrap
+ * the result in an object to be able to distinguish success from failure.
+ */
