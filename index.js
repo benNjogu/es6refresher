564dbd317c4154c67d871065d87a04a0eb7382a6
benNjogu <kamandemachafi@gmail.com>
2022-09-02 12:08:21 +0300

    Buggy

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-/**
- * Automated testing is the process of writing a program that tests another program
- */
+function numberToString(n, base = 10) {
+  let result = "",
+    sign = "";
+  if (n < 0) {
+    sign = "-";
+    n = -n;
+  }
 
-function test(label, body){
-  if(!body()) console.log(`Failed: ${label}`);
+  do {
+    result = String(n % base) + result;
+    n /= base;
+  } while (n > 0);
+
+  return sign + result;
 }
 
-test("Convert Latin text to uppercase", () => {
-  return "hello".toUpperCase() == "HELLO"
-});
-
-test("Convert Greek text to uppercase", () => {
-  return "Χαίρετε".toUpperCase() == "XAIPETE"
-});//-> Failed: Convert Greek text to uppercase
-
-console.log("Χαίρετε".toUpperCase());//-> ΧΑΊΡΕΤΕ
-
-let sum = (a, b) => a + b;
-
-//given
-let num1 = 0;
-let num2;
-let total = num1 + num2;
-//when
-//then
-
-function test(label, body) {
-  if (!body()) console.log(`Failed: ${label}`);
-}
-
-test("sum of two nums", () => {
-  return sum(num1, num2) == total;
-});
+console.log(numberToString(13, 10));
