f9259858d6b37b137788374cb5fb32fc9bd00ca7
benNjogu <kamandemachafi@gmail.com>
2022-09-02 11:40:48 +0300

    Testing sum function

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ test("Convert Greek text to uppercase", () => {
 });//-> Failed: Convert Greek text to uppercase
 
 console.log("Χαίρετε".toUpperCase());//-> ΧΑΊΡΕΤΕ
+
+let sum = (a, b) => a + b;
+
+//given
+let num1 = 0;
+let num2;
+let total = num1 + num2;
+//when
+//then
+
+function test(label, body) {
+  if (!body()) console.log(`Failed: ${label}`);
+}
+
+test("sum of two nums", () => {
+  return sum(num1, num2) == total;
+});
