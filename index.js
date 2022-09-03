e61ac4144a88a74ff5eb08969601137b7a6dbb5d
benNjogu <kamandemachafi@gmail.com>
2022-09-03 21:53:08 +0300

    Assertions

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-function promptDirection(question) {
-  let result = prompt(question);
-  if (result.toLowerCase() == "left") return "L";
-  if (result.toLowerCase() == "right") return "R";
-  throw new Error("Invalid direction: " + result);
-}
-class InputError extends Error {}
+/**
+ * Assertions:
+ * -Checks inside a program that verify that something is the way it is supposed to be.
+ * -Used to find programmer mistakes.
+ */
 
-function promptDirection(question) {
-  let result = prompt(question);
-  if (result.toLowerCase() == "left") return "L";
-  if (result.toLowerCase() == "right") return "R";
-  throw new InputError("Invalid direction: " + result);
-}
-
-for (;;) {
-  //This kind of loop creates a loop that does not terminate on it's own.
-  try {
-    let dir = promtDirection("Where");//-> Typo now recognized.
-    console.log("You chose ", dir);
-    break;
-  } catch (e) {
-    if (e instanceof InputError) {
-      console.log("Not a valid direction. Try again");
-    } else {
-      throw e;
-    }
+function firstElement(array) {
+  if (array.length == 0) {
+    throw new Error("firstElement called with []"); //This makes it easy to find the cause of mistakes when they occur
   }
+
+  return array[0];
 }
+
+console.log(firstElement([1, 2, 3, 4])); //-> 1
+console.log(firstElement([])); //-> Uncaught Error: firstElement called with []
+
+/**
+ * @NB
+ * Don't use assertions everywhere in your code. Use them for mistakes that are easy to make or
+ * that you find yourself making, else, your code will be very noisy!!
+ */
