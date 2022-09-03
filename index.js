32e9e2204b814fe15f5074a3a9be7cca0f1b1e4a
benNjogu <kamandemachafi@gmail.com>
2022-09-03 17:10:53 +0300

    Poor code

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-/**
- * Exceptions - mechanism that makes it possible for code that runs into a problem
- * to raise(or throw) an exception.
- * An exception can be any value.
- */
-function promptDirection(question) {
-  let result = prompt(question);
-  if (result.toLowerCase() == "left") return "L";
-  if (result.toLowerCase() == "right") return "R";
-  throw new Error("Invalid direction: " + result);
-}
+let accounts = {
+  a: 100,
+  b: 0,
+  c: 20,
+};
 
-function look() {
-  if (promptDirection("which way?") == "L") {
-    return "a house";
-  } else {
-    return "two angry bears";
+function getAccount() {
+  let accountName = prompt("Enter account name");
+  if (!accounts.hasOwnProperty(accountName)) {
+    throw new Error(`No such account: ${accountName}`);
   }
+
+  return accountName;
 }
 
-try {
-  console.log("You see " + look()); //-> You see two angry bears
-} catch (error) {
-  console.log("Something went wrong: " + error); //-> Something went wrong: Error: Invalid direction: r
+function transfer(from, amount) {
+  if (accounts[from] < amount) return;
+  accounts[from] -= amount;
+  accounts[getAccount()] += amount;
 }
+
+transfer("a", 63);
+console.log(accounts); //-> { a: 37, b: 63, c: 20 }
+
+/*
+The above transfer function transfers a sum of money from a given account to
+another, asking for the name of the other account in the process. If given an
+invalid account name, getAccount throws an exception.
+But transfer first removes the money from the account and then calls
+getAccount before it adds it to another account. If it is broken off by an
+exception at that point, it’ll just make the money disappear.
+That code could have been written a little more intelligently, for example by
+calling getAccount before it starts moving money around. 
+*/
