731458f4cbbe21e4cc3164f01f57333daa288a17
benNjogu <kamandemachafi@gmail.com>
2022-09-03 21:39:08 +0300

    Selective catching.

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ function promptDirection(question) {
   if (result.toLowerCase() == "right") return "R";
   throw new Error("Invalid direction: " + result);
 }
+class InputError extends Error {}
+
+function promptDirection(question) {
+  let result = prompt(question);
+  if (result.toLowerCase() == "left") return "L";
+  if (result.toLowerCase() == "right") return "R";
+  throw new InputError("Invalid direction: " + result);
+}
 
 for (;;) {
   //This kind of loop creates a loop that does not terminate on it's own.
   try {
-    let dir = promptDirection("Where");//mispelling promptDirection may cause a big problem!
+    let dir = promtDirection("Where");//-> Typo now recognized.
     console.log("You chose ", dir);
     break;
   } catch (e) {
-    console.log("Not a valid direction. Try again.");
+    if (e instanceof InputError) {
+      console.log("Not a valid direction. Try again");
+    } else {
+      throw e;
+    }
   }
 }
