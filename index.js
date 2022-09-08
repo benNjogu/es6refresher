b154d7c6ab8ae0b8a454fcdfee8612add2800fe1
benNjogu <kamandemachafi@gmail.com>
2022-09-08 12:22:20 +0300

    Matches

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-let cartoonCrying = /boo+(hoo)+/i;
-console.log(cartoonCrying.test("Boohoooohoohooo")); //-> true
+let match = /\d+/.exec("one two 100");
+/**
+ * An exec method will return null if no match was found and return an object with information about the match
+ * otherwise
+ */
+console.log(match); //-> [ "100" ]
+/**
+ * The exec method has an index property that tells us where in the string the successful match begins.
+ */
+console.log(match.index); //-> 8
