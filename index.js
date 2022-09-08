71ca2d9ec6ac14587d8594c1fc6694d7d7a086b2
benNjogu <kamandemachafi@gmail.com>
2022-09-08 09:04:47 +0300

    Set of characters pt1

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log(/abc/.test("abcde")); //-> true
-console.log(/abc/.test("abxde")); //-> false
-console.log(/abc/.test("abxdabce")); //-> true
+/**
+ * Putting a set of characters between square brackets makes that part of the expression match any of the
+ * characters in the brackets.
+ */
+console.log(/[0123456789]/.test("in 1992")); //-> true
+
+/**
+ * Within square brackets, a hyphen between two characters can be used to indicate a range of characters,
+ * where the ordering is determined by the character's unicode number.
+ */
+console.log(/[0-9]/.test("in 1992")); //-> true
