486960406ca8384a43c0aac802e76f22dac23c4b
benNjogu <kamandemachafi@gmail.com>
2022-09-08 12:48:36 +0300

    groups

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-let match = /\d+/.exec("one two 100");
-/**
- * An exec method will return null if no match was found and return an object with information about the match
- * otherwise
- */
-console.log(match); //-> [ "100" ]
-/**
- * The exec method has an index property that tells us where in the string the successful match begins.
- */
-console.log(match.index); //-> 8
+let quotedText = /'([^']*)'/;
+console.log(quotedText.exec("She said 'hello'")); //-> [ "'hello'", "hello" ]
