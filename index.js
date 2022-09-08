71e048dfd53a9384723eaf8187c89f3f0f58cbd2
benNjogu <kamandemachafi@gmail.com>
2022-09-08 08:42:36 +0300

    testing for matches

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-let re1 = new RegExp("abc");
-let re2 = /abc/;
-
-console.log(re1); //-> /abc/
-console.log(re2); //-> /abc/
-
-/**
- * Some characters such as question marks and plus signs, have special meanings in regular expressions
- * and must be preceeded by a backslash if they are meant to represent the character itself.
- */
-let eighteenPlus = /eighteen\+/;
-console.log(eighteenPlus);
+console.log(/abc/.test("abcde")); //-> true
+console.log(/abc/.test("abxde")); //-> false
+console.log(/abc/.test("abxdabce")); //-> true
