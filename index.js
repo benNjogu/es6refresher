a1f9c06da85ba3bb14ea3bf2bf0c4ec73942de02
benNjogu <kamandemachafi@gmail.com>
2022-09-08 08:34:37 +0300

    RegularExpression basics

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-/**
- * Assertions:
- * -Checks inside a program that verify that something is the way it is supposed to be.
- * -Used to find programmer mistakes.
- */
+let re1 = new RegExp("abc");
+let re2 = /abc/;
 
-function firstElement(array) {
-  if (array.length == 0) {
-    throw new Error("firstElement called with []"); //This makes it easy to find the cause of mistakes when they occur
-  }
-
-  return array[0];
-}
-
-console.log(firstElement([1, 2, 3, 4])); //-> 1
-console.log(firstElement([])); //-> Uncaught Error: firstElement called with []
+console.log(re1); //-> /abc/
+console.log(re2); //-> /abc/
 
 /**
- * @NB
- * Don't use assertions everywhere in your code. Use them for mistakes that are easy to make or
- * that you find yourself making, else, your code will be very noisy!!
+ * Some characters such as question marks and plus signs, have special meanings in regular expressions
+ * and must be preceeded by a backslash if they are meant to represent the character itself.
  */
+let eighteenPlus = /eighteen\+/;
+console.log(eighteenPlus);
