ac1a9e5788a8034de9406705ea5dee5b4f185887
benNjogu <kamandemachafi@gmail.com>
2022-09-08 11:16:51 +0300

    Grouping subexpressions

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-/**
- * To indicate that a pattern should occur a precise number of times, use braces.
- * e.g. {4} indicates that an element should occur four times.
- *      {2, 4} means an element must occur at least two times at most four times.
- */
-
-let dateTime = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
-console.log(dateTime.test("1-30-2003 8:45"));//-> true
-
-/**
- * You can also specify open-ended ranges when using braces by omitting the number after the comma
- * e.g. {5,} means five or more times.
- */
+let cartoonCrying = /boo+(hoo)+/i;
+console.log(cartoonCrying.test("Boohoooohoohooo")); //-> true
