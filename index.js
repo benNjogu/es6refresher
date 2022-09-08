9108cd07727b4c083178ed5e41e0aca5b598167c
benNjogu <kamandemachafi@gmail.com>
2022-09-08 13:20:57 +0300

    The date class

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-let quotedText = /'([^']*)'/;
-console.log(quotedText.exec("She said 'hello'")); //-> [ "'hello'", "hello" ]
+console.log(new Date()); //-> Thu Sep 08 2022 13:01:27 GMT+0300 (East Africa Time)
+console.log(new Date(2009, 11, 9)); //-> Wed Dec 09 2009 00:00:00 GMT+0300 (East Africa Time)
+/**
+ * NB: JS uses a convention where the month numbers start at zero and day numbers start at one.
+ */
+
+console.log(new Date(2009, 11, 9).getTime()); //-> 1260306000000
+console.log(new Date(1260306000000)); //-> Date Wed Dec 09 2009 00:00:00 GMT+0300 (East Africa Time)
+
+function getDate(string) {
+  let [_, month, day, year] = /(\d{1,2})-(\d{1,2})-(\d{4})/.exec(string);
+  return new Date(year, month - 1, day);
+}
+console.log(getDate("1-30-2003")); //-> Thu Jan 30 2003 00:00:00 GMT+0300 (East Africa Time)
