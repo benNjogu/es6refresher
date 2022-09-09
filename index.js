4a69d09decf187cee84a7c3c5306d67808c5c3fa
benNjogu <kamandemachafi@gmail.com>
2022-09-09 16:24:59 +0300

    Improvised modules

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-console.log("  word".search(/\S/)); //-> 2
-console.log("  ".search(/\S/)); //-> -1
-/**
- * The search method as used above, returns the first index on which the expression is found
- * or -1 when it wasn't found.
- */
+const weekDay = (function () {
+  const names = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return {
+    name(number) {
+      return names[number];
+    },
+    number(name) {
+      return names.indexOf(name);
+    },
+  };
+})();
+
+console.log(weekDay.name(1)); //-> Monday
+console.log(weekDay.number("Sunday")); //-> 0
