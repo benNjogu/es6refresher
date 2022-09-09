ec9ee9717148eb3b0f75cb0b7923595e875d48ec
benNjogu <kamandemachafi@gmail.com>
2022-09-09 11:28:49 +0300

    replace with a function

--- index.js
+++ index.js
@@ -4,3 +4,17 @@
 
 let s = "the cia and the fbi";
 console.log(s.replace(/\b(fbi|cia)\b/g, (str) => str.toUpperCase())); //-> the CIA and the FBI
+
+let stock = "1 lemon, 2 cabbages, and 101 eggs";
+function minusOne(match, amount, unit) {
+  amount = Number(amount) - 1;
+  if (amount == 1) {
+    //only one left, remove the 's'
+    unit = unit.slice(0, unit.length - 1);
+  } else if (amount == 0) {
+    amount = "no";
+  }
+  return amount + " " + unit;
+}
+
+console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
