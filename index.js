42465aa1da24c6417649ca12348fd8ac31e47e2e
benNjogu <kamandemachafi@gmail.com>
2022-09-09 10:16:17 +0300

    Word and string boundaries

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-console.log(new Date()); //-> Thu Sep 08 2022 13:01:27 GMT+0300 (East Africa Time)
-console.log(new Date(2009, 11, 9)); //-> Wed Dec 09 2009 00:00:00 GMT+0300 (East Africa Time)
+console.log(/cat/.test("concatenate")); //-> true
+
 /**
- * NB: JS uses a convention where the month numbers start at zero and day numbers start at one.
+ * A word boundary(\b) can be the start or end of a string or any point in the string that has a word character
+ * as in (\w) on one side and a non word character on the other.
  */
-
-console.log(new Date(2009, 11, 9).getTime()); //-> 1260306000000
-console.log(new Date(1260306000000)); //-> Date Wed Dec 09 2009 00:00:00 GMT+0300 (East Africa Time)
-
-function getDate(string) {
-  let [_, month, day, year] = /(\d{1,2})-(\d{1,2})-(\d{4})/.exec(string);
-  return new Date(year, month - 1, day);
-}
-console.log(getDate("1-30-2003")); //-> Thu Jan 30 2003 00:00:00 GMT+0300 (East Africa Time)
+console.log(/\bcat\b/.test("concatenate")); //-> false
