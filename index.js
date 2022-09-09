6f19c8d5818b87d245b02b1cb772ea478f086a02
benNjogu <kamandemachafi@gmail.com>
2022-09-09 10:48:12 +0300

    replace using regular expressions

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("papa".replace("p", "m")); //-> mapa
+
+/**
+ * Using regular expressions all matches in the string can be replaced,
+ * using a g option(for global).
+ */
+console.log("papa".replace(/[p]/g, "m")); //-> mama
