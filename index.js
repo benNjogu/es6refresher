6eaa3947a3172d02dccf32e7cab3614f00ca4888
benNjogu <kamandemachafi@gmail.com>
2022-09-09 16:35:12 +0300

    Evaluating data as code

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-const weekDay = (function () {
-  const names = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+const x = 1;
+function evalAndReturnX(code) {
+  eval(code); //executes a string in the current scope
+  return x;
+}
 
-  return {
-    name(number) {
-      return names[number];
-    },
-    number(name) {
-      return names.indexOf(name);
-    },
-  };
-})();
-
-console.log(weekDay.name(1)); //-> Monday
-console.log(weekDay.number("Sunday")); //-> 0
+console.log(evalAndReturnX("var x = 2")); //-> 2
+console.log(x); //-> 1
