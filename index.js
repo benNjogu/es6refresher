77bb6c07ad07e940c5402f679b428928da74ada2
benNjogu <kamandemachafi@gmail.com>
2022-09-09 11:53:05 +0300

    greed

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
+/**
+ * If you put a question mark after greedy operators they become non-greedy(+?, *?, ??, {}?)
+ */
 function stripComments(code) {
-  return code.replace(/\/\/.*|\/\*[^]*\*\//g, "");
+  return code.replace(/\/\/.*|\/\*[^]*?\*\//g, "");
 }
 
-console.log(stripComments("1 + /*2*/3")); //-> 1 + 3
-console.log(stripComments("x = 10;// ten!"));// → x = 10;
-
-/**
- *The below code is giving a wrong output due to the greedy nature of the repetition operators(+, *, ?, {}) 
- */
-console.log(stripComments("1 /* a */+/* b */ 1"));// → 1 1
-
-/**
- * The part b4 the or operator, matches 2 slashes character followed by any number of non newline characters.
- * The part for multiline comments use[^](any character that is not on the empty set of characters) as a way to
- * match any character.
- * We cannot just use a period because block comments can continue on a new line, and the period character does 
- * not match newline characters.
- */
+console.log(stripComments("1 /* a */+/* b */ 1")); // → 1 + 1
