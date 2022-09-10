bf1334b4185150e80d2310d5480db271af409380
benNjogu <kamandemachafi@gmail.com>
2022-09-10 11:07:20 +0300

    The event loop

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-function* powers(n) {
-  for (let current = n; ; current *= n) {
-    yield current;
-  }
+try {
+  setTimeout(() => {
+    throw new Error("Woosh"); //-> Uncaught Error: Woosh
+  }, 20);
+} catch (_) {
+  //This will not run
+  console.log("Caught!");
 }
 
-for (let power of powers(3)) {
-  if (power > 50) break;
-  console.log(power);
-}
+let start = Date.now();
+setTimeout(() => {
+  console.log("Timeout ran at ", Date.now() - start); //-> Timeout ran at  999
+}, 20);
+while (Date.now() < start + 50) {}
+console.log("Wasted time until", Date.now() - start); //-> Wasted time until 50
+
+Promise.resolve("Done").then(console.log);
+console.log("Me first!");
 /**
-3 
-9 
-27
+ * Me first!
+ * Done
  */
