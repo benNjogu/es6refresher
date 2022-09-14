91b8027310d1eb5134a8d5ed62d2630c745921d0
benNjogu <kamandemachafi@gmail.com>
2022-09-14 13:27:32 +0300

    createTextNode

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let paragraphs = document.body.getElementsByTagName("p");
-document.body.insertBefore(paragraphs[2], paragraphs[0]);
+function replaceImages() {
+  let images = document.body.getElementsByTagName("img");
+  for (let i = images.length - 1; i >= 0; i--) {
+    let image = images[i];
+    if (image.alt) {
+      let text = document.createTextNode(image.alt);
+      image.parentNode.replaceChild(text, image);
+    }
+  }
+} //-> The Cat in the Hat.
+
 /**
-Third
-
-First
-
-Second
+ * Given a string, createTextNode, gives us a text node that we can insert into the document to make
+ * it show up on the screen.
  */
