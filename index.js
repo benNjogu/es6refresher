05cc67f5abaa10fb0d7e5dea376ab902d0a2178b
benNjogu <kamandemachafi@gmail.com>
2022-09-14 12:11:07 +0300

    getting element by ID

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-let link = document.body.getElementsByTagName("a")[0];
-console.log(link.href); //-> http://127.0.0.1:5500/Click%20Me
+let name = document.getElementById("myName");
+console.log(name.src); //-> http://127.0.0.1:5500/img/ben.png
