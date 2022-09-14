ea5bd38ad6858008223ab0ce903262a790df698f
benNjogu <kamandemachafi@gmail.com>
2022-09-14 14:17:05 +0300

    Attributes

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-function elt(type, ...children) {
-  let node = document.createElement(type);
-  for (let child of children) {
-    if (typeof child != "string") node.appendChild(child);
-    else node.appendChild(document.createTextNode(child));
+let paras = document.body.getElementsByTagName("p");
+for (let para of Array.from(paras)) {
+  if (para.getAttribute("data-classified") == "secret") {
+    para.remove();
   }
-  return node;
 }
-
-document
-  .getElementById("quote")
-  .appendChild(
-    elt(
-      "footer",
-      "-",
-      elt("strong", "Karl Popper"),
-      ", preface to the second edition of ",
-      elt("em", "The Open Socitety and Its Enemies"),
-      ", 1950"
-    )
-  );
 /**
-    No book can ever be finished. While working on it we learn just enough to find it immature the moment we turn away from it.
-    -Karl Popper, preface to the second edition of The Open Socitety and Its Enemies, 1950
-   */
+ * I have two feet.
+ */
