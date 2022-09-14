6eb05b270da9c0f53067c21f56849b415e97aa7b
benNjogu <kamandemachafi@gmail.com>
2022-09-14 14:03:25 +0300

    Creating nodes

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-let arrayish = { 0: "one", 1: "two", length: 2 };
-let array = Array.from(arrayish);
-console.log(array.map((s) => s.toUpperCase())); //-> [ "ONE", "TWO" ]
+function elt(type, ...children) {
+  let node = document.createElement(type);
+  for (let child of children) {
+    if (typeof child != "string") node.appendChild(child);
+    else node.appendChild(document.createTextNode(child));
+  }
+  return node;
+}
+
+document
+  .getElementById("quote")
+  .appendChild(
+    elt(
+      "footer",
+      "-",
+      elt("strong", "Karl Popper"),
+      ", preface to the second edition of ",
+      elt("em", "The Open Socitety and Its Enemies"),
+      ", 1950"
+    )
+  );
+/**
+    No book can ever be finished. While working on it we learn just enough to find it immature the moment we turn away from it.
+    -Karl Popper, preface to the second edition of The Open Socitety and Its Enemies, 1950
+   */
