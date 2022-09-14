f08499738dc60188968670610b622cbaa4b6226e
benNjogu <kamandemachafi@gmail.com>
2022-09-14 11:58:43 +0300

    getting element by tag name

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-try {
-  setTimeout(() => {
-    throw new Error("Woosh"); //-> Uncaught Error: Woosh
-  }, 20);
-} catch (_) {
-  //This will not run
-  console.log("Caught!");
-}
-
-let start = Date.now();
-setTimeout(() => {
-  console.log("Timeout ran at ", Date.now() - start); //-> Timeout ran at  999
-}, 20);
-while (Date.now() < start + 50) {}
-console.log("Wasted time until", Date.now() - start); //-> Wasted time until 50
-
-Promise.resolve("Done").then(console.log);
-console.log("Me first!");
-/**
- * Me first!
- * Done
- */
+let link = document.body.getElementsByTagName("a")[0];
+console.log(link.href); //-> http://127.0.0.1:5500/Click%20Me
