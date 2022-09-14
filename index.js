c255048f5f6042e2c1fb4ecbfa074c0f3834e716
benNjogu <kamandemachafi@gmail.com>
2022-09-14 13:33:55 +0300

    convert live nodes to a solid collection

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-function replaceImages() {
-  let images = document.body.getElementsByTagName("img");
-  for (let i = images.length - 1; i >= 0; i--) {
-    let image = images[i];
-    if (image.alt) {
-      let text = document.createTextNode(image.alt);
-      image.parentNode.replaceChild(text, image);
-    }
-  }
-} //-> The Cat in the Hat.
-
-/**
- * Given a string, createTextNode, gives us a text node that we can insert into the document to make
- * it show up on the screen.
- */
+let arrayish = { 0: "one", 1: "two", length: 2 };
+let array = Array.from(arrayish);
+console.log(array.map((s) => s.toUpperCase())); //-> [ "ONE", "TWO" ]
