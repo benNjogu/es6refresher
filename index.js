2294ec96afffb38803702bcbae9ee59250d4604a
benNjogu <kamandemachafi@gmail.com>
2022-09-14 18:01:49 +0300

    static, relative, absolute

--- index.js
+++
@@ -1,8 +0,0 @@
-function count(selector) {
-  return document.querySelectorAll(selector).length;
-}
-
-console.log(count("p")); //-> 4
-console.log(count(".animal")); //-> 2
-console.log(count("p.animal")); //-> 0
-console.log(count("p > .animal")); //-> 1
