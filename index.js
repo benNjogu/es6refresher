863813eefe44407f283cef311092f29ff269ea34
benNjogu <kamandemachafi@gmail.com>
2022-09-15 11:21:30 +0300

    Mouse clicks

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-window.addEventListener("keydown", (event) => {
-  if (event.key == "v") {
-    document.body.style.background = "violet";
-  }
-});
-window.addEventListener("keyup", (event) => {
-  if (event.key == "v") {
-    document.body.style.background = "";
-  }
-});
-
-window.addEventListener("keydown", (event) => {
-  if (event.key == " " && event.ctrlKey) console.log("Continuing!");
+window.addEventListener("click", (event) => {
+  let dot = document.createElement("div");
+  dot.className = "dot";
+  dot.style.left = event.pageX - 4 + "px";
+  dot.style.top = event.pageY - 4 + "px";
+  document.body.appendChild(dot);
 });
