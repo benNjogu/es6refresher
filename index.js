e12ab2032fe6225c7c41540f9aa93f3b061de9f3
benNjogu <kamandemachafi@gmail.com>
2022-09-15 11:50:09 +0300

    Mouse motions

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-window.addEventListener("click", (event) => {
-  let dot = document.createElement("div");
-  dot.className = "dot";
-  dot.style.left = event.pageX - 4 + "px";
-  dot.style.top = event.pageY - 4 + "px";
-  document.body.appendChild(dot);
+let lastX; //Tracks the last observed mouse X position
+let bar = document.querySelector("div");
+bar.addEventListener("mousedown", (event) => {
+  if (event.button == 0) {
+    lastX = event.clientX;
+    window.addEventListener("mousemove", moved);
+    event.preventDefault(); //prevent selection
+  }
 });
+function moved(event) {
+  if (event.buttons == 0) {
+    window.removeEventListener("mousemove", moved);
+  } else {
+    let dist = event.clientX - lastX;
+    let newWidth = Math.max(10, bar.offsetWidth + dist);
+    bar.style.width = newWidth + "px";
+    lastX = event.clientX;
+  }
+}
