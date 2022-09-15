e043594ba439aec289c42c6433bf5e482cd054ad
benNjogu <kamandemachafi@gmail.com>
2022-09-15 17:59:18 +0300

    Reading multuple files

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let input = document.querySelector("input");
 input.addEventListener("change", () => {
-  if (input.files.length > 0) {
-    let file = input.files[0];
-    console.log("You chose", file.name);
-    if (file.type) console.log("it has type", file.type);
+  for (let file of Array.from(input.files)) {
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+      console.log("File", file.name, "starts with", reader.result.slice(0, 20));
+    });
+    reader.readAsText(file);
   }
 });
