eb7fe07a336f7e61d7f07a1ad0308204f6f8bbf7
benNjogu <kamandemachafi@gmail.com>
2022-09-15 18:11:25 +0300

    Local storage

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-let input = document.querySelector("input");
-input.addEventListener("change", () => {
-  for (let file of Array.from(input.files)) {
-    let reader = new FileReader();
-    reader.addEventListener("load", () => {
-      console.log("File", file.name, "starts with", reader.result.slice(0, 20));
-    });
-    reader.readAsText(file);
-  }
-});
+localStorage.setItem("userName", "Bernad");
+console.log(localStorage.getItem("userName")); //-> Bernad
+localStorage.removeItem("userName");
