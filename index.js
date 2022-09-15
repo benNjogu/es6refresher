5e26645dba0b4ef9ee356d2713c647adc4bb3ef0
benNjogu <kamandemachafi@gmail.com>
2022-09-15 14:00:29 +0300

    Debouncing

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-//Setting timeout
-let bombTimer = setTimeout(() => {
-  console.log("BOOM");
-}, 500);
+let textArea = document.querySelector("textarea");
+let timeout;
+textArea.addEventListener("input", () => {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => console.log("Typed!"), 500);
+});
 
-//Clearing timeout
-if (Math.random() < 0.5) {
-  console.log("Defused.");
-  clearTimeout(bombTimer);
-}
-
-let ticks = 0;
-let clock = setInterval(() => {
-  console.log("tick", ticks++);
-  if (ticks == 10) {
-    clearInterval(clock);
-    console.log("stop.");
+//checking mouse positions every 250 milliseconds
+let scheduled = null;
+window.addEventListener("mousemove", (event) => {
+  if (!scheduled) {
+    setTimeout(() => {
+      document.body.textContent = `Mouse at ${scheduled.pageX}, ${scheduled.pageY}`;
+      scheduled = null;
+    }, 250);
   }
-}, 200);
+  scheduled = event;
+});
