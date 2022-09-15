beeb727b322de60d997aef6a44d979b191ff1424
benNjogu <kamandemachafi@gmail.com>
2022-09-15 17:53:46 +0300

    Files fields part 1

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-let textArea = document.querySelector("textarea");
-let timeout;
-textArea.addEventListener("input", () => {
-  clearTimeout(timeout);
-  timeout = setTimeout(() => console.log("Typed!"), 500);
-});
-
-//checking mouse positions every 250 milliseconds
-let scheduled = null;
-window.addEventListener("mousemove", (event) => {
-  if (!scheduled) {
-    setTimeout(() => {
-      document.body.textContent = `Mouse at ${scheduled.pageX}, ${scheduled.pageY}`;
-      scheduled = null;
-    }, 250);
+let input = document.querySelector("input");
+input.addEventListener("change", () => {
+  if (input.files.length > 0) {
+    let file = input.files[0];
+    console.log("You chose", file.name);
+    if (file.type) console.log("it has type", file.type);
   }
-  scheduled = event;
 });
