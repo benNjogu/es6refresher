bf23b410d5176c594ecaed8f52c6e18e5544c3b0
benNjogu <kamandemachafi@gmail.com>
2022-09-16 14:54:33 +0300

    default exports

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-import { Teacher } from "./teacher";
+import Teacher from "./teacher";
 
 let t = new Teacher("Ben", "BSc.");
 console.log(t.name); //-> Ben
 console.log(t.degree); //-> BSc
 t.walk(); //-> walking
 t.teach(); //-> teaching
+
+/**
+ * with named imports -> import ... from
+ * with default imports -> import {...} from
+ */
--- teacher.js
+++ teacher.js
@@ -1,7 +1,7 @@
 import { Person } from "./person";
 
-export function promote() {}
-export class Teacher extends Person {
+//export function promote() {}
+export default class Teacher extends Person {
   constructor(name, degree) {
     super(name);
     this.degree = degree;
