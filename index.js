9ec01e8a74b2f4f21d6be7cff77a224422d3d832
benNjogu <kamandemachafi@gmail.com>
2022-09-16 14:41:29 +0300

    Modules

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
-
-  walk() {
-    console.log("walking");
-  }
-}
-
-class Teacher extends Person {
-  constructor(name, degree) {
-    super(name);
-    this.degree = degree;
-  }
-
-  teach() {
-    console.log("teaching");
-  }
-}
+import { Teacher } from "./teacher";
 
 let t = new Teacher("Ben", "BSc.");
 console.log(t.name); //-> Ben
